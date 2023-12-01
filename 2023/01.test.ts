e33f3c6f1f7d7dc2ca6715d373eabfa42e52b0d3
harryalaw <harryalaw@gmail.com>
2023-12-01 14:10:10 +0000

    Part 1 working :D

--- 2023/01.test.ts
+++ 2023/01.test.ts
@@ -1,13 +1,13 @@
 import { describe, it, expectTypeOf } from "vitest"
-import type { Parse, Part1 } from "./01.ts"
+import type { GetNumbers1, Part1 } from "./01.ts"
 
 describe("Part1", () => {
     it("Should find the numbers", () => {
-        type B = Parse<'pqr3stu8vwx'>
-        expectTypeOf<Parse<'1abc2'>>().toEqualTypeOf<12>();
-        expectTypeOf<Parse<'pqr3stu8vwx'>>().toEqualTypeOf<38>();
-        expectTypeOf<Parse<'a1b2c3d4e5f'>>().toEqualTypeOf<15>();
-        expectTypeOf<Parse<'treb7uchet'>>().toEqualTypeOf<77>();
+        type B = GetNumbers1<'pqr3stu8vwx'>
+        expectTypeOf<GetNumbers1<'1abc2'>>().toEqualTypeOf<12>();
+        expectTypeOf<GetNumbers1<'pqr3stu8vwx'>>().toEqualTypeOf<38>();
+        expectTypeOf<GetNumbers1<'a1b2c3d4e5f'>>().toEqualTypeOf<15>();
+        expectTypeOf<GetNumbers1<'treb7uchet'>>().toEqualTypeOf<77>();
     })
 
     it("Should sum it all good", () => {
@@ -16,7 +16,7 @@ pqr3stu8vwx
 a1b2c3d4e5f
 treb7uchet`
         type Ans = Part1<Input>
-        expectTypeOf<Part1<Input>>().toEqualTypeOf<142>();
+        expectTypeOf<Ans>().toEqualTypeOf<142>();
     })
 
 });
--- 2023/01.ts
+++ 2023/01.ts
@@ -1,24 +1,38 @@
+import { ComputeSimple } from "../util/helpers/Equality";
 import { Add } from "../util/numbers/Add";
-import { ParseInt } from "../util/numbers/numbers";
+import { GreaterThan } from "../util/numbers/Comparisons";
+import { Digit, DigitsToNumber } from "../util/numbers/numbers";
 
-export type Parse<T> = ParseInt<`${FirstNumber<T>}${LastNumber<T>}`>;
+import type { Input } from "./Inputs/01"
+type Solve1 = Part1<Input>
 
-export type Part1<T> = SolvePart1<T>;
+export type Part1<T> = SolvePart1<T> extends [infer Total extends number, infer Remaining]
+    ? Add<Total, SolvePart1<Remaining>>
+    : SolvePart1<T>
 
-
-type SolvePart1<Lines, Total extends number = 0> = NextLine<Lines> extends [infer Line, infer Rest]
-    ? SolvePart1<Rest, Add<Parse<Line>, Total>>
-    : Add<Parse<Lines>, Total>
-
-type FirstNumber<T> = T extends `${infer First}${infer Rest}`
-    ? First extends `${number}` ? First : FirstNumber<Rest> : never
-
-type Reverse<T> = T extends `${infer First}${infer Rest}`
-    ? `${Reverse<Rest>}${First}` : T
-
-type LastNumber<T> = FirstNumber<Reverse<T>>
+type SolvePart1<Lines, Total extends number = 0, Depth extends number = 0, MaxDepth = 999> =
+    Depth extends MaxDepth ? [Total, Lines] :
+    NextLine<Lines> extends [infer Line, infer Rest]
+    ? SolvePart1<Rest, Add<GetNumbers1<Line>, Total>, Add<Depth, 1>>
+    : Add<GetNumbers1<Lines>, Total>
 
 type NextLine<T> = T extends `${infer First}\n${infer Rest}`
     ? [First, Rest]
     : [T];
+
+export type GetNumbers1<
+    Line,
+    Values extends { first: [number, Digit], last: [number, Digit] } = { first: [100, 0], last: [0, 0] },
+    Depth extends number = 0
+> = Line extends `${infer First}${infer Rest}`
+        ? First extends `${infer N extends Digit}`
+            ? [GreaterThan<Values["first"][0], Depth>, GreaterThan<Values["last"][0], Depth>] extends infer Result
+                ? Result extends [true, true] ? GetNumbers1<Rest, ComputeSimple<Omit<Values, "first"> & { first: [Depth, N] } | never>, Add<Depth, 1>>
+                : Result extends [true, false] ? GetNumbers1<Rest, { first: [Depth, N], last: [Depth, N] }, Add<Depth, 1>>
+                : Result extends [false, true] ? GetNumbers1<Rest, Values, Add<Depth, 1>>
+                : Result extends [false, false] ? GetNumbers1<Rest, ComputeSimple<Omit<Values, "last"> & { last: [Depth, N] }>, Add<Depth, 1>>
+                : never
+            : never
+        : GetNumbers1<Rest, Values, Add<Depth, 1>>
+    : DigitsToNumber<[Values["first"][1], Values["last"][1]]>
 
