e293f272b1ea9acff203638717fee7fa13a76d7c
harryalaw <harryalaw@gmail.com>
2023-12-01 15:09:38 +0000

    Bundling?

---
+++ util/index.ts
@@ -0,0 +1,9 @@
+import type { Add } from "./numbers/Add.ts"
+import type { GreaterThan } from "./numbers/Comparisons.ts"
+import type {Digit, DigitsToNumber } from "./numbers/numbers"
+
+import type { ComputeSimple } from "./helpers/Equality.ts"
+
+export {
+    Add, GreaterThan, Digit, DigitsToNumber, ComputeSimple
+}
